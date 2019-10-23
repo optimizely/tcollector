a5c04e5e90477a79332164f8f4a47b5f7e2797ab
Atul Jha <atul.jha@optimizely.com>
2019-10-23 13:50:24 -0700

    Updating kafka eet staging bootstrap server (#100)

--- collectors/0/samza_staging_eet.py
+++ collectors/0/samza_staging_eet.py
@@ -6,11 +6,7 @@ from collectors.lib.samza_metric_reporter import SamzaMetricReporter
 CONSUMER_GROUP_ID = "tcollector_samza_staging_eet"
 
 KAFKA_BOOTSTRAP_SERVERS = [
-    "1.kafka-eet.us-east-1.backend-staging.optimizely:9094",
-    "2.kafka-eet.us-east-1.backend-staging.optimizely:9094",
-    "3.kafka-eet.us-east-1.backend-staging.optimizely:9094",
-    "4.kafka-eet.us-east-1.backend-staging.optimizely:9094",
-    "5.kafka-eet.us-east-1.backend-staging.optimizely:9094",
+    "kafka-eet-staging.us-east-1.optimizely:9094"
 ]
 
 
