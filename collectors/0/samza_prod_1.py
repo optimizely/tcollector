bfa1a836bc1497a64370877919e2958de6202d61
Atul Jha <atul.jha@optimizely.com>
2020-01-07 11:11:02 -0800

    Adding separate tcollector for samza KeyValueStoreMetrics (#103)

--- collectors/0/samza_prod_1.py
+++ collectors/0/samza_prod_1.py
@@ -3,7 +3,7 @@ import sys
 
 from collectors.lib.samza_metric_reporter import SamzaMetricReporter
 
-CONSUMER_GROUP_ID = "tcollector_samza_prod"
+CONSUMER_GROUP_ID = "tcollector_samza_prod_1"
 
 # FIXME: Should we set this as an ENV?
 KAFKA_BOOTSTRAP_SERVERS = [
@@ -15,13 +15,9 @@ KAFKA_BOOTSTRAP_SERVERS = [
     'kafkaprod-6:9092'
 ]
 
-METRIC_CLASSES = ['org.apache.samza.system.kafka.KafkaSystemConsumerMetrics',
-                  'org.apache.samza.metrics.JvmMetrics',
-                  'org.apache.samza.container.SamzaContainerMetrics']
-
 
 def main():
-    reporter = SamzaMetricReporter(CONSUMER_GROUP_ID, KAFKA_BOOTSTRAP_SERVERS, metric_classes=METRIC_CLASSES)
+    reporter = SamzaMetricReporter(CONSUMER_GROUP_ID, KAFKA_BOOTSTRAP_SERVERS, metric_classes=['org.apache.samza.storage.kv.KeyValueStoreMetrics'])
     reporter.run()
 
 
