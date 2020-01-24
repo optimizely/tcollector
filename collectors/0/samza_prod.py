11b424a97ffca77626db71ead47c46d0e790f381
Atul Jha <atul.jha@optimizely.com>
2020-01-24 11:19:36 -0800

    Reverting PR#103 (#104)
    * Revert "Adding separate tcollector for samza KeyValueStoreMetrics (#103)"
    
    This reverts commit bfa1a836bc1497a64370877919e2958de6202d61.
    
    * Removing kv metrics

--- collectors/0/samza_prod.py
+++ collectors/0/samza_prod.py
@@ -15,15 +15,10 @@ KAFKA_BOOTSTRAP_SERVERS = [
     'kafkaprod-6:9092'
 ]
 
-METRIC_CLASSES = ['org.apache.samza.system.kafka.KafkaSystemConsumerMetrics',
-                  'org.apache.samza.metrics.JvmMetrics',
-                  'org.apache.samza.container.SamzaContainerMetrics']
-
-
 def main():
-    reporter = SamzaMetricReporter(CONSUMER_GROUP_ID, KAFKA_BOOTSTRAP_SERVERS, metric_classes=METRIC_CLASSES)
-    reporter.run()
 
+    reporter = SamzaMetricReporter(CONSUMER_GROUP_ID, KAFKA_BOOTSTRAP_SERVERS)
+    reporter.run()
 
 if __name__ == "__main__":
     sys.exit(main())
--- collectors/lib/samza_metric_reporter.py
+++ collectors/lib/samza_metric_reporter.py
@@ -2,15 +2,11 @@ import json
 import re
 import sys
 import socket
+import utils
 
 from kafka import KafkaConsumer
 
 from collectors.lib import utils
-
-METRIC_CLASSES = ['org.apache.samza.system.kafka.KafkaSystemConsumerMetrics',
-                  'org.apache.samza.metrics.JvmMetrics',
-                  'org.apache.samza.container.SamzaContainerMetrics',
-                  'org.apache.samza.storage.kv.KeyValueStoreMetrics']
 
 
 class SamzaMetricReporter:
@@ -23,14 +19,12 @@ class SamzaMetricReporter:
     SAMZA_CONSUMER_LAG_METRIC_NAME = 'samza.consumer.lag'
     CONSUMER_LAG_PATTERN = re.compile(r'kafka(?:_(?:input|output))?-(.+)-(\d+)-messages-behind-high-watermark')
 
-    def __init__(self, consumer_group_id, kafka_bootstrap_servers, kafka_cluster_prefix='', kafka_metrics_topic='samza_metrics', metric_classes=None):
-        if metric_classes is None:
-            metric_classes = METRIC_CLASSES
+    def __init__(self, consumer_group_id, kafka_bootstrap_servers, kafka_cluster_prefix='', kafka_metrics_topic='samza_metrics'):
         utils.drop_privileges()
         self.consumer = KafkaConsumer(kafka_metrics_topic,
                                       group_id=consumer_group_id,
                                       bootstrap_servers=kafka_bootstrap_servers)
-        self.metric_classes = metric_classes
+        self.methods_to_run = [self.report_consumer_lag, self.report_jvm_and_container_metrics]
         self.kafka_cluster_prefix = kafka_cluster_prefix
 
     def run(self):
@@ -64,11 +58,16 @@ class SamzaMetricReporter:
             metrics_raw = message_json['metrics']
             header_raw = message_json['header']
 
-            self.report_metrics(metrics_raw, header_raw, self.metric_classes)
+            # reporting logic is specific to the type of metric reported
+            for method in self.methods_to_run:
+                method(metrics_raw, header_raw)
 
-    def report_metrics(self, metrics_raw, header_raw, metric_classes):
+    def report_jvm_and_container_metrics(self, metrics_raw, header_raw):
+
         metrics = {}
-        for m in metric_classes:
+        for m in ['org.apache.samza.metrics.JvmMetrics',
+            'org.apache.samza.container.SamzaContainerMetrics']:
+
             if m in metrics_raw:
                 metrics[m] = metrics_raw[m]
 
@@ -77,44 +76,56 @@ class SamzaMetricReporter:
 
         for metric_type, metric_map in metrics.iteritems():
             if metric_type == 'org.apache.samza.storage.kv.KeyValueStoreMetrics':
-                # add `source` to distinguish partitions on the same container
+                # add `source` to distiguish partitions on the same container
                 tags['source'] = header_raw['source'].strip().replace(' ', '-').lower()
 
             for metric_name, metric_val in metric_map.iteritems():
-                # report consumer lag
-                if metric_type == 'org.apache.samza.system.kafka.KafkaSystemConsumerMetrics':
-                    m = self.CONSUMER_LAG_PATTERN.match(metric_name)
-                    if m:
-                        # Kafka topic can be extracted from the metric name. This is needed when
-                        # a Samza job consumes from multiple sources.
-                        tags['topic'] = m.group(1)
-                        # Partition number is a part of the metric name when reported to Kafka.
-                        # Include it as a tag instead so that the metric can be aggregated.
-                        tags['partition'] = m.group(2)
-
-                        self.print_consumer_lag(
-                            ts,
-                            metric_val,
-                            tags)
-                else:
-                    self.print_jvm_and_container_metric(self.sanitize(metric_type),
-                                                        self.sanitize(metric_name),
-                                                        ts,
-                                                        metric_val,
-                                                        tags)
+                self.print_jvm_and_container_metric(self.sanitize(metric_type),
+                                                    self.sanitize(metric_name),
+                                                    ts,
+                                                    metric_val,
+                                                    tags)
                 sys.stdout.flush()
+
+    def report_consumer_lag(self, metrics_raw, header_raw):
+
+        m = 'org.apache.samza.system.kafka.KafkaSystemConsumerMetrics'
+
+        if m in metrics_raw:
+            metric = metrics_raw[m]
+            tags = self.create_standard_tags(header_raw)
+            ts = int(header_raw['time'] / 1000)
+
+            for metric_name, metric_val in metric.iteritems():
+
+                m = self.CONSUMER_LAG_PATTERN.match(metric_name)
+                if m:
+                    # Kafka topic can be extracted from the metric name. This is needed when
+                    # a Samza job consumes from multiple sources.
+                    tags['topic'] = m.group(1)
+                    # Partition number is a part of the metric name when reported to Kafka.
+                    # Include it as a tag instead so that the metric can be aggregated.
+                    tags['partition'] = m.group(2)
+
+                    self.print_consumer_lag(
+                        ts,
+                        metric_val,
+                        tags)
+                sys.stdout.flush()
+
 
     def print_jvm_and_container_metric(self, metric_type, metric_name, ts, value, tags):
         if self.is_number(value):
             print ("%s.%s %d %s %s" %
-                   (metric_type.replace('org.apache.', ''), metric_name, ts, value, self.to_tsdb_tag_str(tags)))
+                (metric_type.replace('org.apache.', ''), metric_name, ts, value, self.to_tsdb_tag_str(tags)))
 
     def print_consumer_lag(self, ts, value, tags):
         if self.is_number(value):
             print ("%s%s %d %s %s" % (self.kafka_cluster_prefix, self.SAMZA_CONSUMER_LAG_METRIC_NAME, ts, value, self.to_tsdb_tag_str(tags)))
             # Send to DataDog
-            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
             sock.sendto("%s:%s|g|#%s" % (self.SAMZA_CONSUMER_LAG_METRIC_NAME, value, self.to_dd_tag_str(tags)), ("localhost", 8125))
+
 
     def to_dd_tag_str(self, tags):
         if tags:
@@ -125,6 +136,7 @@ class SamzaMetricReporter:
 
         return tags_str
 
+
     def to_tsdb_tag_str(self, tags):
         if tags:
             tags_str = " " + " ".join("%s=%s" % (self.sanitize(name), v)
@@ -133,6 +145,7 @@ class SamzaMetricReporter:
             tags_str = ""
 
         return tags_str
+
 
     @staticmethod
     def create_standard_tags(header_raw):
